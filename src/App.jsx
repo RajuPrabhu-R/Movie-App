872e15b0ec9f4b68afe63fd0ce4f140cdd64470f
Raju Prabhu R <143985509+RajuPrabhu-R@users.noreply.github.com>
2025-08-30 12:12:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,8 @@ const MovieCard = ({ movie, onClick }) => (
     onClick={() => onClick(movie)}
   >
     <img
-      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+      src={poster_path ?
+`https://image.tmdb.org/t/p/w500${movie.poster_path}` : 'No-Poster.png'}
       alt={movie.title || movie.name}
       className="w-full h-64 object-cover"
     />
